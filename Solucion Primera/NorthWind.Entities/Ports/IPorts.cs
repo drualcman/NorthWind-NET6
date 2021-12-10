d53f2b772c3ab6c38d816d023f1111bf119cc1b1
Sergi Ortiz Gómez <drualcman@msn.com>
2021-12-10 10:40:58 +0800

    definicion de las interfaces a utilizar en los puestos para los casos de uso

---
+++ Solucion Primera/NorthWind.Entities/Ports/IPorts.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.Ports
+{
+    /// <summary>
+    /// Puerto de entrada de datos
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IPort<T>
+    {
+        /// <summary>
+        /// Manejar los datos de entrada
+        /// </summary>
+        /// <param name="dtp"></param>
+        /// <returns></returns>
+        ValueTask Handle(T dtp);
+    }
+
+    /// <summary>
+    /// Puerto sin datos de entrada
+    /// </summary>
+    public interface IPort
+    {
+
+        /// <summary>
+        /// Manejar la entrada sin datos
+        /// </summary>
+        /// <returns></returns>
+        ValueTask Handle();
+    }
+}
