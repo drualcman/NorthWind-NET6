b2bb73852219b8fbfe6340e636574dabe12def46
Sergi Ortiz Gómez <drualcman@msn.com>
2021-12-10 11:03:06 +0800

    preparando las especificaciones

---
+++ Solucion Primera/NorthWind.Entities/Abstractions/Specification.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Entities.Abstractions
+{
+    /// <summary>
+    /// Definir especificaciones
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class Specification<T>
+    {
+        /// <summary>
+        /// Condicion que vamos a evaluar
+        /// </summary>
+        public abstract Expression<Func<T, bool>> ConditionExpression { get; }
+        
+        /// <summary>
+        /// Ejecutar la condicion
+        /// </summary>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        public bool IsSatisfiedBy(T entity)
+        {
+            Func<T, bool> expressionDelegate = ConditionExpression.Compile();
+            return expressionDelegate(entity);
+        }
+    }
+}
