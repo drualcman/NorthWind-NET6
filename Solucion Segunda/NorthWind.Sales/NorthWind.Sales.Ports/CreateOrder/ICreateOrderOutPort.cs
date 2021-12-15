a9bac0816bcabae9a2e0683c7b530313db40a794
Sergi Ortiz Gómez <drualcman@msn.com>
2021-12-15 18:54:56 +0800

    agregados los puertos de comunicacion

---
+++ Solucion Segunda/NorthWind.Sales/NorthWind.Sales.Ports/CreateOrder/ICreateOrderOutPort.cs
@@ -0,0 +1,16 @@
+﻿using NorthWind.Entities.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Sales.Ports.CreateOrder
+{
+    /// <summary>
+    /// Abstraccion para la devolucion de datos
+    /// </summary>
+    public interface ICreateOrderOutPort : IPort<int>
+    {
+    }
+}
