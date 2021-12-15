e946b7aa0b96f81341bdd63be099da95930e3860
Sergi Ortiz Gómez <drualcman@msn.com>
2021-12-15 18:57:26 +0800

    agregado comentarios y heredar de IUnitOfWork en el ILogRepository que se me quedo atras

--- Solucion Primera/NorthWind.Entities/Interfaces/ILogWritableRepository.cs
+++ Solucion Primera/NorthWind.Entities/Interfaces/ILogWritableRepository.cs
@@ -10,9 +10,17 @@ namespace NorthWind.Entities.Interfaces
     /// <summary>
     /// Escribir en un registro de errores
     /// </summary>
-    public interface ILogWritableRepository
+    public interface ILogWritableRepository : IUnitOfWork
     {
+        /// <summary>
+        /// agregar un registro por medio de clase
+        /// </summary>
+        /// <param name="log"></param>
         void Add(Log log);
+        /// <summary>
+        /// agregar un registro directamente como texto
+        /// </summary>
+        /// <param name="message"></param>
         void Add(string message);
     }
 }
