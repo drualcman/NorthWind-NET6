5faec53a45168554c5e6e577beea4f45adf2c0b3
Sergi Ortiz Gómez <drualcman@msn.com>
2021-12-15 18:17:28 +0800

    agregados los eventos

---
+++ Solucion Segunda/NorthWind.Sales/NorthWind.Sales.Events/SpecialOrderCreatedEvent.cs
@@ -0,0 +1,36 @@
+﻿using NorthWind.Entities.Interfaces.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Sales.Events
+{
+    /// <summary>
+    /// Implementacion del evento para enviar una notificacion
+    /// Esto es como un EventArgs en un evento de WindowsForm
+    /// </summary>
+    public class SpecialOrderCreatedEvent : IDomainEvent
+    {
+        /// <summary>
+        /// Identificador de la orden creada
+        /// </summary>
+        public int OrderId { get; set; }
+        /// <summary>
+        /// Cuenta de los productos de la order
+        /// </summary>
+        public int ProductsCount { get; set; }
+
+        /// <summary>
+        /// Constructor para la creacion del evento
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <param name="productsCount"></param>
+        public SpecialOrderCreatedEvent(int orderId, int productsCount)
+        {
+            OrderId = orderId;
+            ProductsCount = productsCount;
+        }
+    }
+}
---
+++ Solucion Segunda/NorthWind.Sales/NorthWind.Sales.Events/SpecialOrderCreatedEventHandler.cs
@@ -0,0 +1,35 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.Interfaces.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Sales.Events
+{
+    /// <summary>
+    /// Manejador de la implementacion del evento del dominio
+    /// </summary>
+    public class SpecialOrderCreatedEventHandler : IDomainEventHandler<SpecialOrderCreatedEvent>
+    {
+
+        readonly IMailService MailService;
+
+        public SpecialOrderCreatedEventHandler(IMailService mailService)
+        {
+            MailService = mailService;
+        }
+
+
+        /// <summary>               
+        /// recibir los datos para enviar el correo de la orden especial
+        /// </summary>
+        /// <param name="orderCreated"></param>
+        /// <exception cref="NotImplementedException"></exception>
+        public void Handle(SpecialOrderCreatedEvent orderCreated)
+        {
+            MailService.Send($"Ordern {orderCreated.OrderId} con {orderCreated.ProductsCount} productos ha sido creada");
+        }
+    }
+}
