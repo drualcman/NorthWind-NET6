e6ccf205946e5e8e12a8e07ed38413103a89c2dd
Sergi Ortiz Gómez <drualcman@msn.com>
2021-12-17 10:16:31 +0800

    agregado el debug de aplicacion

---
+++ Solucion Primera/NorthWind.Loggers/DebugStatusLogger.cs
@@ -0,0 +1,16 @@
+﻿using NorthWind.Entities.Interfaces;
+using System.Diagnostics;
+
+namespace NorthWind.Loggers
+{
+    /// <summary>
+    /// Log para enviar a la ventana de debug
+    /// </summary>
+    public class DebugStatusLogger : IApplicationStatusLogger
+    {
+        public void Log(string message)
+        {
+            Debug.Write(message);
+        }
+    }
+}
