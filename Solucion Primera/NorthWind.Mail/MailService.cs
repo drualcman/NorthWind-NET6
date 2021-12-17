cb4975cc0c4ea503bb2259bde86f4cb569f10f1e
Sergi Ortiz Gómez <drualcman@msn.com>
2021-12-17 10:13:36 +0800

    agregado el servicio de correo

---
+++ Solucion Primera/NorthWind.Mail/MailService.cs
@@ -0,0 +1,33 @@
+﻿using NorthWind.Entities.Interfaces;
+
+namespace NorthWind.Mail
+{
+    /// <summary>
+    /// Implementacion para enviar correos electronicos
+    /// </summary>
+    public class MailService : IMailService
+    {
+        readonly IApplicationStatusLogger Logger;
+
+        /// <summary>
+        /// Constructor que recibe el application log para notificar
+        /// </summary>
+        /// <param name="logger"></param>
+        public MailService(IApplicationStatusLogger logger)
+        {
+            Logger = logger;
+        }
+
+        /// <summary>
+        /// Enviar un correo electronico con el mensaje enviado
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public ValueTask Send(string message)
+        {
+            Logger.Log($"*** MailService: {message} ***");
+            Logger.Log($"*** MailService: Servidor de correo no configurado ***");
+            return ValueTask.CompletedTask;
+        }
+    }
+}
