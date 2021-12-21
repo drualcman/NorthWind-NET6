1cbdafe15c3272c7903231412559435ee07c052c
Sergi Ortiz Gómez <drualcman@msn.com>
2021-12-21 12:16:28 +0800

    creado el view model para el presentador

---
+++ Solucion Segunda/NorthWind.Sales/NorthWind.Sales.ViewModels/CreateOrderViewModel.cs
@@ -0,0 +1,22 @@
+﻿namespace NorthWind.Sales.ViewModels
+{
+    /// <summary>
+    /// mostrar la orden creada
+    /// </summary>
+    public class CreateOrderViewModel
+    {
+        /// <summary>
+        /// numero de orden
+        /// </summary>
+        public int OrderId { get; set; }
+
+        /// <summary>
+        /// constructor 
+        /// </summary>
+        /// <param name="orderId"></param>
+        public CreateOrderViewModel(int orderId)
+        {
+            OrderId = orderId;
+        }
+    }
+}
