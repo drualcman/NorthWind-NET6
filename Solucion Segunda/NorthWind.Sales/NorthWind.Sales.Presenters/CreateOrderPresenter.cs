b865264f7bb1937072769c4ba869c2de1411c504
Sergi Ortiz Gómez <drualcman@msn.com>
2021-12-21 12:16:07 +0800

    creado el presentador

---
+++ Solucion Segunda/NorthWind.Sales/NorthWind.Sales.Presenters/CreateOrderPresenter.cs
@@ -0,0 +1,20 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Sales.Ports.CreateOrder;
+using NorthWind.Sales.ViewModels;
+
+namespace NorthWind.Sales.Presenters
+{
+    public class CreateOrderPresenter : ICreateOrderOutPort, IPresenter<CreateOrderViewModel>
+    {
+        /// <summary>
+        /// contenido de la orden
+        /// </summary>
+        public CreateOrderViewModel Content { get; private set; }
+
+        public ValueTask Handle(int orderId)
+        {
+            Content = new CreateOrderViewModel(orderId);
+            return ValueTask.CompletedTask;
+        }
+    }
+}
---
+++ Solucion Segunda/NorthWind.Sales/NorthWind.Sales.Presenters/DependencyContainer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using NorthWind.Entities.Interfaces.Events;
+using NorthWind.Entities.Interfaces.Validation;
+using NorthWind.Entities.Services;
+using NorthWind.Sales.Ports.CreateOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWind.Sales.Presenters
+{
+    /// <summary>
+    /// Helper para injectar los servicios implementados dentro del projecto
+    /// </summary>
+    public static class DependencyContainer
+    {
+        /// <summary>
+        /// Agregar todos servicios del projecto de entities
+        /// </summary>
+        /// <param name="services"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddPresenters(this IServiceCollection services)
+        {
+            services.AddScoped<ICreateOrderOutPort, CreateOrderPresenter>();
+            return services;
+        }
+    }
+}
